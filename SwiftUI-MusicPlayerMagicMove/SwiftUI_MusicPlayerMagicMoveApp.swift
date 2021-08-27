9b7fcb024d51d26ddab628d52c2759f5d0dd4392
Arpit Dixit <arpitd@dewsolutions.in>
2021-08-28 01:48:08 +0530

    Initial Commit

---
+++ SwiftUI-MusicPlayerMagicMove/SwiftUI_MusicPlayerMagicMoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MusicPlayerMagicMoveApp.swift
+//  SwiftUI-MusicPlayerMagicMove
+//
+//  Created by Arpit Dixit on 28/08/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MusicPlayerMagicMoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
